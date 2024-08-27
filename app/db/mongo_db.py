8fca2b5f79cc437004fd25ad45b98e104e7cc467
aiaccfin <aiaccfin@gmail.com>
2024-08-27 14:16:35 -0400

    v2.CC save to Mongo

--- app/db/mongo_db.py
+++ app/db/mongo_db.py
@@ -1,43 +1,41 @@
 import pymongo, dotenv, streamlit as st
 CFG = dotenv.dotenv_values(".env")
 
+try:
+    mongodb_client = pymongo.MongoClient(CFG["MONGO_STATEMENT_URI"])
+    db = mongodb_client[CFG["DB_STATEMENT"]]
+    collection_bank = db[CFG["COLLECTION_BANK"]]
+    collection_cc = db[CFG["COLLECTION_CC"]]
+    st.write("Connected to MongoDB successfully.")
+except Exception as e:
+    print(f"Failed to connect to MongoDB: {e}")
+
+
 def create_index():
-    mongodb_client = pymongo.MongoClient(CFG["ATLAS_URI"])
-    db = mongodb_client[CFG["DB_NAME"]]
-    collection = db[CFG["COL_NAME"]]
-    collection.create_index(
-        [('clientID', pymongo.ASCENDING), ('date', pymongo.ASCENDING), ('description', pymongo.ASCENDING),
-         ('amount', pymongo.ASCENDING)],
+    collection_cc.create_index(
+        [('clientID', pymongo.ASCENDING), ('date', pymongo.ASCENDING), ('description', pymongo.ASCENDING), ('amount', pymongo.ASCENDING)],
+        unique=True
+    )
+
+    collection_bank.create_index(
+        [('clientID', pymongo.ASCENDING), ('date', pymongo.ASCENDING), ('description', pymongo.ASCENDING),('amount', pymongo.ASCENDING)],
         unique=True
     )
 
 
-def save_to_mongodb(data):
-    # Connect to MongoDB
-    collection =''
-    db = ''
-    try:
-        mongodb_client = pymongo.MongoClient(CFG["ATLAS_URI"])
-        db = mongodb_client[CFG["DB_NAME"]]
-        collection = db[CFG["COL_NAME"]]
-        st.write("Connected to MongoDB successfully.")
-
-    except Exception as e:
-        print(f"Failed to connect to MongoDB: {e}")
-
+def save_to_cc(data):
     try:
         # Insert data into MongoDB
         if isinstance(data, list):
             st.write(1)
-            collection.insert_many(data)
+            collection_cc.insert_many(data)
         else:
             st.write(2)
-            collection.insert_one(data)
+            collection_cc.insert_one(data)
     except Exception as e:
         st.error(f"Already in DB.")
 
-
-    print(f"Data successfully inserted into {db}.{collection} collection.")
+    print(f"Data successfully inserted into {db}.{collection_cc} collection.")
 
 
 def check_mongodb_connection():
@@ -57,5 +55,3 @@ def check_mongodb_connection():
 
     except Exception as e:
         print(f"An error occurred: {e}")
-
-
--- pages/1_📈Dashboard for Accountant.py
+++ pages/1_📈Dashboard for Accountant.py
@@ -7,18 +7,16 @@ streamlit_components.streamlit_ui('🦣 Dashboard for Accountant')
 from app.data_entry import email_processing, pdf_processing
 from app.db import mongo_db
 # -----------------------------------------------------------------------------------------------------------
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Data Entry: Bank Statement", "Data Entry: Receipt", "Data Entry: Email", "Data Entry: Manual", "DB Structure"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Data Entry: CC", "Data Entry: Bank Statement", "Data Entry: Email", "Data Entry: Manual", "DB Structure"])
 
 with tab3:
     if button("Extract from Email", key="button3"):    email_processing.email_extraction()
 
 with tab1:
-    if button("extract PDF?", key="button1"):
-        # Example usage
-        pdf_path = "./data/bs/USA/Sample-Bank Statement-US/Bank Statement-BOA Checking (4760)-Tea Hill-20230101-20231231/eStmt_2023-02-28.pdf"  # Path to your PDF file
+    if button("CC?", key="button1"):
+        pdf_path = "./data/bs/USA/Sample-Credit Card Statement-US/Credit Card-AmEx 91009-Tea Hill-2023/2023-01-09.pdf"
         extracted_data = pdf_processing.extract_pdf_lines(pdf_path)
-        # print(extracted_data)  # Print extracted data to see the format
-        st.write(extracted_data)  # Print extracted data to see the format
+        st.write(extracted_data)
 
         if button("Regex?", key="button12"):
             transactions = []
@@ -26,16 +24,19 @@ with tab1:
                 text = entry['text']
                 # Regex to match transaction lines (e.g., date and amount at the end)
                 # match = re.match(r'(\d{2}/\d{2}/\d{2}) (.+) (\d+,\d+\.\d{2})$', text)
-                match = re.match(r'(\d{2}/\d{2}/\d{2}) (.+) (-?\d+,\d+\.\d{2})$', text)
+                # match = re.match(r'(\d{2}/\d{2}/\d{2}) (.+) (-?\$\d+,\d+\.\d{2})$', text)
+                match = re.match(r'(\d{2}/\d{2}/\d{2}) (.+) (-?\$\d{1,3}(?:,\d{3})*\.\d{2})$', text)
 
                 if match:
                     date, description, amount = match.groups()
+                    amount = amount.replace('$', '')
                     amount = float(amount.replace(',', ''))
                     transactions.append({
-                        'clientID':'C101',
+                        'clientID':'CC888',
                         'date': date,
                         'description': description,
                         'amount': amount
                     })
 
-            mongo_db.save_to_mongodb(transactions)
+            st.write(transactions)
+            mongo_db.save_to_cc(transactions)
